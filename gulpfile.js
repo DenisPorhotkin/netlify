e15f9b8c4fa78421250111aa651fa7d58bb99322
Porhotkin <porhotkin@yandex.ru>
2025-03-31 00:20:30 +0300

    подготовили к публикации

--- gulpfile.js
+++ gulpfile.js
@@ -4,53 +4,53 @@ const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+}
+
 function html() {
-    return gulp.src('src/**/*.html') // тут нам нужно описать, что делает функция
+  return gulp.src('src/**/*.html')
         .pipe(plumber())
-        .pipe(gulp.dest('dist/'))
+				.pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({stream: true}));
 }
 
 function css() {
-   return gulp.src('src/blocks/**/*.css')
+  return gulp.src('src/blocks/**/*.css')
         .pipe(plumber())
         .pipe(concat('bundle.css'))
-        .pipe(gulp.dest('dist/'))
+				.pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({stream: true}));
 }
 
 function images() {
-    return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
-        .pipe(gulp.dest('dist/images'))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.reload({stream: true}));
 }
-  
+
 function clean() {
-    return del('dist');
+  return del('dist');
 }
 
 function watchFiles() {
-    gulp.watch(['src/**/*.html'], html);
-    gulp.watch(['src/blocks/**/*.css'], css);
-    gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
-}
-
-function serve() {
-    browserSync.init({
-      server: {
-        baseDir: './dist'
-      }
-    });
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
 
 const build = gulp.series(clean, gulp.parallel(html, css, images));
-const watchapp = gulp.parallel(build, watchFiles, serve); 
+const watchapp = gulp.parallel(build, watchFiles, serve);
 
-exports.watchapp = watchapp;
-exports.build = build;
-exports.default = watchapp;
-
-exports.clean = clean;
-exports.html = html // строчка, которая позволит вызвать эту задачу из терминала 
+exports.html = html;
 exports.css = css;
 exports.images = images;
+exports.clean = clean;
+
+exports.build = build;
+exports.watchapp = watchapp;
+exports.default = watchapp;
